af484809fa3fa5120aac426d2131f644c3167ae9
Svilen M. Varbanov <svilen_varbanov@msn.com>
2024-10-18 08:59:15 +0300

    CIS2 Instance base class.

---
+++ STEPViewer/CIS2/CIS2Instance.cpp
@@ -0,0 +1,252 @@
+#include "stdafx.h"
+#include "CIS2Instance.h"
+#include "CIS2Model.h"
+
+#include <math.h>
+
+// ************************************************************************************************
+CCIS2Instance::CCIS2Instance(int64_t iID, SdaiInstance iSdaiInstance, enumCISInstanceType enCISInstanceType)
+	: _geometry(iID, iSdaiInstance, true)
+	, CInstanceBase()
+	, m_enCISInstanceType(enCISInstanceType)
+	, m_iExpressID(internalGetP21Line(iSdaiInstance))
+	, m_bReferenced(false)
+{
+	ASSERT(iSdaiInstance != 0);
+	ASSERT(m_iExpressID != 0);
+
+	Calculate();
+}
+
+CCIS2Instance::~CCIS2Instance()
+{}
+
+/*virtual*/ OwlModel CCIS2Instance::getModel() const /*override*/
+{
+	OwlModel iOwlModel = 0;
+	owlGetModel(GetModel(), &iOwlModel);
+	ASSERT(iOwlModel != 0);
+
+	return iOwlModel;
+}
+
+/*virtual*/ int64_t CCIS2Instance::calculateInstance(int64_t* piVertexBufferSize, int64_t* piIndexBufferSize) /*override*/
+{
+	assert(piVertexBufferSize != nullptr);
+	assert(piIndexBufferSize != nullptr);
+
+	*piVertexBufferSize = *piIndexBufferSize = 0;
+
+	SdaiModel iSdaiModel = sdaiGetInstanceModel((int_t)m_iInstance);
+
+	int64_t iOwlInstance = 0;
+	owlBuildInstance(iSdaiModel, (int_t)m_iInstance, &iOwlInstance);
+
+	CalculateInstance(iOwlInstance, piVertexBufferSize, piIndexBufferSize, nullptr);
+
+	return iOwlInstance;
+}
+
+void CCIS2Instance::CalculateMinMax(
+	float& fXmin, float& fXmax, 
+	float& fYmin, float& fYmax, 
+	float& fZmin, float& fZmax)
+{
+	
+	if (!HasGeometry())
+	{
+		return;
+	}
+
+	const uint32_t VERTEX_LENGTH = getVertexLength();
+
+	// Triangles
+	if (!m_vecTriangles.empty())
+	{
+		for (size_t iPrimitive = 0; iPrimitive < m_vecTriangles.size(); iPrimitive++)
+		{
+			for (int64_t iIndex = m_vecTriangles[iPrimitive].startIndex();
+				iIndex < m_vecTriangles[iPrimitive].startIndex() + m_vecTriangles[iPrimitive].indicesCount();
+				iIndex++)
+			{
+				fXmin = (float)fmin(fXmin, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 0]);
+				fXmax = (float)fmax(fXmax, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 0]);
+				fYmin = (float)fmin(fYmin, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 1]);
+				fYmax = (float)fmax(fYmax, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 1]);
+				fZmin = (float)fmin(fZmin, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 2]);
+				fZmax = (float)fmax(fZmax, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 2]);
+			} // for (size_t iIndex = ...
+		} // for (size_t iPrimitive = ...
+	} // if (!m_vecTriangles.empty())	
+
+	// Conceptual faces polygons
+	if (!m_vecConcFacePolygons.empty())
+	{
+		for (size_t iPrimitive = 0; iPrimitive < m_vecConcFacePolygons.size(); iPrimitive++)
+		{
+			for (int64_t iIndex = m_vecConcFacePolygons[iPrimitive].startIndex();
+				iIndex < m_vecConcFacePolygons[iPrimitive].startIndex() + m_vecConcFacePolygons[iPrimitive].indicesCount();
+				iIndex++)
+			{
+				if ((m_pIndexBuffer->data()[iIndex] == -1) || (m_pIndexBuffer->data()[iIndex] == -2))
+				{
+					continue;
+				}
+
+				fXmin = (float)fmin(fXmin, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 0]);
+				fXmax = (float)fmax(fXmax, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 0]);
+				fYmin = (float)fmin(fYmin, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 1]);
+				fYmax = (float)fmax(fYmax, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 1]);
+				fZmin = (float)fmin(fZmin, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 2]);
+				fZmax = (float)fmax(fZmax, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 2]);
+			} // for (size_t iIndex = ...
+		} // for (size_t iPrimitive = ...
+	} // if (!m_vecConcFacePolygons.empty())
+
+	// Lines
+	if (!m_vecLines.empty())
+	{
+		for (size_t iPrimitive = 0; iPrimitive < m_vecLines.size(); iPrimitive++)
+		{
+			for (int64_t iIndex = m_vecLines[iPrimitive].startIndex();
+				iIndex < m_vecLines[iPrimitive].startIndex() + m_vecLines[iPrimitive].indicesCount();
+				iIndex++)
+			{
+				if (m_pIndexBuffer->data()[iIndex] == -1)
+				{
+					continue;
+				}
+
+				fXmin = (float)fmin(fXmin, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 0]);
+				fXmax = (float)fmax(fXmax, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 0]);
+				fYmin = (float)fmin(fYmin, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 1]);
+				fYmax = (float)fmax(fYmax, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 1]);
+				fZmin = (float)fmin(fZmin, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 2]);
+				fZmax = (float)fmax(fZmax, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 2]);
+			} // for (size_t iIndex = ...
+		} // for (size_t iPrimitive = ...
+	} // if (!m_vecLines.empty())
+
+	// Points
+	if (!m_vecPoints.empty())
+	{
+		for (size_t iPrimitive = 0; iPrimitive < m_vecPoints.size(); iPrimitive++)
+		{
+			for (int64_t iIndex = m_vecPoints[iPrimitive].startIndex();
+				iIndex < m_vecPoints[iPrimitive].startIndex() + m_vecPoints[iPrimitive].indicesCount();
+				iIndex++)
+			{
+				fXmin = (float)fmin(fXmin, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 0]);
+				fXmax = (float)fmax(fXmax, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 0]);
+				fYmin = (float)fmin(fYmin, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 1]);
+				fYmax = (float)fmax(fYmax, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 1]);
+				fZmin = (float)fmin(fZmin, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 2]);
+				fZmax = (float)fmax(fZmax, m_pVertexBuffer->data()[(m_pIndexBuffer->data()[iIndex] * VERTEX_LENGTH) + 2]);
+			} // for (size_t iIndex = ...
+		} // for (size_t iPrimitive = ...
+	} // if (!m_vecPoints.empty())
+}
+
+void CCIS2Instance::Scale(float fScaleFactor)
+{
+	_geometry::scale(fScaleFactor);
+}
+
+void CCIS2Instance::Calculate()
+{
+	// Format
+	setSTEPFormatSettings();
+
+	// Extra settings
+	const int_t flagbit1 = 2;
+	setFilter(GetModel(), flagbit1, flagbit1);
+	setSegmentation(GetModel(), 16, 0.);
+
+	/* Geometry */
+
+	ASSERT(m_pVertexBuffer == nullptr);
+	m_pVertexBuffer = new _vertices_f(getVertexLength());
+
+	ASSERT(m_pIndexBuffer == nullptr);
+	m_pIndexBuffer = new _indices_i32();
+
+	calculate(m_pVertexBuffer, m_pIndexBuffer);
+
+	MATERIALS mapMaterial2ConcFaces;
+	MATERIALS mapMaterial2ConcFaceLines;
+	MATERIALS mapMaterial2ConcFacePoints;
+
+	m_iConceptualFacesCount = GetConceptualFaceCnt(m_iInstance);
+	for (int64_t iConceptualFaceIndex = 0; iConceptualFaceIndex < m_iConceptualFacesCount; iConceptualFaceIndex++)
+	{
+		int64_t iStartIndexTriangles = 0;
+		int64_t iIndicesCountTriangles = 0;
+		int64_t iStartIndexLines = 0;
+		int64_t iIndicesCountLines = 0;
+		int64_t iStartIndexPoints = 0;
+		int64_t iIndicesCountPoints = 0;
+		int64_t iStartIndexConceptualFacePolygons = 0;
+		int64_t iIndicesCountConceptualFacePolygons = 0;
+		ConceptualFace iConceptualFace = GetConceptualFaceEx(
+			m_iInstance,
+			iConceptualFaceIndex,
+			&iStartIndexTriangles, &iIndicesCountTriangles,
+			&iStartIndexLines, &iIndicesCountLines,
+			&iStartIndexPoints, &iIndicesCountPoints,
+			0, 0,
+			&iStartIndexConceptualFacePolygons, &iIndicesCountConceptualFacePolygons);
+
+		/* Material */
+
+		uint32_t iAmbientColor = CCIS2Model::DEFAULT_COLOR;
+		uint32_t iDiffuseColor = CCIS2Model::DEFAULT_COLOR;
+		uint32_t iEmissiveColor = CCIS2Model::DEFAULT_COLOR;
+		uint32_t iSpecularColor = CCIS2Model::DEFAULT_COLOR;
+		float fTransparency = 1.f;
+
+		OwlInstance iMaterialInstance = GetConceptualFaceMaterial(iConceptualFace);
+		if (iMaterialInstance != 0)
+		{
+			iAmbientColor = GetMaterialColorAmbient(iMaterialInstance);
+			iDiffuseColor = GetMaterialColorDiffuse(iMaterialInstance);
+			iEmissiveColor = GetMaterialColorEmissive(iMaterialInstance);
+			iSpecularColor = GetMaterialColorSpecular(iMaterialInstance);
+			fTransparency = (float)COLOR_GET_W(iAmbientColor);
+		}
+
+		_material material(
+			iAmbientColor,
+			iDiffuseColor,
+			iEmissiveColor,
+			iSpecularColor,
+			fTransparency,
+			nullptr);
+
+		if (iIndicesCountTriangles > 0)
+		{
+			addTriangles(iConceptualFaceIndex, iStartIndexTriangles, iIndicesCountTriangles, material, mapMaterial2ConcFaces);
+		}
+
+		if (iIndicesCountConceptualFacePolygons > 0)
+		{
+			addConcFacePolygons(iStartIndexConceptualFacePolygons, iIndicesCountConceptualFacePolygons);
+		}
+
+		if (iIndicesCountLines > 0)
+		{
+			addLines(iConceptualFaceIndex, iStartIndexLines, iIndicesCountLines, material, mapMaterial2ConcFaceLines);
+		}
+
+		if (iIndicesCountPoints > 0)
+		{
+			addPoints(iConceptualFaceIndex, iStartIndexPoints, iIndicesCountPoints, material, mapMaterial2ConcFacePoints);
+		}
+	} // for (int64_t iConceptualFaceIndex = ...	
+
+	// Build the cohorts
+	buildConcFacesCohorts(mapMaterial2ConcFaces, _oglUtils::getIndicesCountLimit());
+	buildConcFacePolygonsCohorts(_oglUtils::getIndicesCountLimit());
+	buildLinesCohorts(mapMaterial2ConcFaceLines, _oglUtils::getIndicesCountLimit());
+	buildLinesCohorts(mapMaterial2ConcFacePoints, _oglUtils::getIndicesCountLimit());
+	buildPointsCohorts(mapMaterial2ConcFacePoints, _oglUtils::getIndicesCountLimit());
+}
---
+++ STEPViewer/CIS2/CIS2Instance.h
@@ -0,0 +1,66 @@
+#ifndef CIS2_INSTANCE_H
+#define CIS2_INSTANCE_H
+
+#include "_oglUtils.h"
+#include "InstanceBase.h"
+
+#include "engine.h"
+
+#include <cstddef>
+#include <string>
+using namespace std;
+
+// ************************************************************************************************
+enum class enumCISInstanceType
+{
+	Instance = 0,
+	DesignPart = 1,
+	Reperesentation = 2,
+};
+
+// ************************************************************************************************
+class CCIS2Instance
+	: public _geometry
+	, public CInstanceBase
+{
+
+private: // Members
+
+	enumCISInstanceType m_enCISInstanceType;
+
+	// Metadata
+	ExpressID m_iExpressID;
+	
+	// UI
+	bool m_bReferenced;
+
+public: // Methods
+	
+	CCIS2Instance(int64_t iID, SdaiInstance iSdaiInstance, enumCISInstanceType enCISInstanceType);
+	virtual ~CCIS2Instance();
+
+	// _geometry
+	virtual OwlModel getModel() const override;
+	virtual int64_t calculateInstance(int64_t* piVertexBufferSize, int64_t* piIndexBufferSize) override;
+
+	// CInstanceBase
+	virtual SdaiInstance GetInstance() const { return (SdaiInstance)m_iInstance; }
+	SdaiModel GetModel() const { return sdaiGetInstanceModel(GetInstance()); }
+	virtual bool HasGeometry() const { return _geometry::hasGeometry(); }
+	virtual bool IsEnabled() const { return getEnable(); }	
+	ExpressID ExpressID() const { return m_iExpressID; }
+	bool& Referenced() { return m_bReferenced; }
+	
+	void CalculateMinMax(
+		float& fXmin, float& fXmax, 
+		float& fYmin, float& fYmax, 
+		float& fZmin, float& fZmax);
+
+	void Scale(float fScaleFactor);
+
+private: // Methods
+	
+	void Calculate();
+};
+
+#endif // CIS2_INSTANCE_H
