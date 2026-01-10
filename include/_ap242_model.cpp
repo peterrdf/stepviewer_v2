0a7cf3fccf9e6a51cad0d4960c0cb23ecf3eab8a
Peter (RDF Ltd.) <peter.bonsma@rdf.bg>
2026-01-10 15:43:05 +0200

    Update _ap242_model.cpp
    Small fix to correctly visualize the file Gantry_step.step

--- include/_ap242_model.cpp
+++ include/_ap242_model.cpp
@@ -192,6 +192,8 @@ void _ap242_model::loadShapeRepresentationItems(_ap242_product_shape_representat
 	assert(pProductShapeRepresentation != nullptr);
 	assert(sdaiRepresentationInstance != 0);
 
+	bool added = false;
+	
 	SdaiAggr sdaiShapeRepresentationRelationshipAggr = sdaiGetEntityExtentBN(getSdaiModel(), "SHAPE_REPRESENTATION_RELATIONSHIP");
 	SdaiInteger shapeRepresentationRelationshipInstancesCnt = sdaiGetMemberCount(sdaiShapeRepresentationRelationshipAggr);
 	if (shapeRepresentationRelationshipInstancesCnt) {
@@ -207,15 +209,18 @@ void _ap242_model::loadShapeRepresentationItems(_ap242_product_shape_representat
 			if (sdaiRep_2Instance && sdaiRep_1Instance != sdaiRep_2Instance &&
 				sdaiRep_1Instance == sdaiRepresentationInstance) {
 				loadRepresentationItems(pProductShapeRepresentation, sdaiRep_2Instance);
+				added = true;
 			}
 
 			if (sdaiRep_1Instance && sdaiRep_1Instance != sdaiRep_2Instance &&
 				sdaiRep_2Instance == sdaiRepresentationInstance) {
 				loadRepresentationItems(pProductShapeRepresentation, sdaiRep_1Instance);
+				added = true;
 			}
 		}
 	}
-	else {
+	
+	if (!added) {
 		loadRepresentationItems(pProductShapeRepresentation, sdaiRepresentationInstance);
 	}
 }
