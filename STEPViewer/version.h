8d8573ff1678d4e2139221a0e6d22d0d4fe2edb9
TzvetiRDF <116556695+TzvetiRDF@users.noreply.github.com>
2026-01-12 14:58:08 +0200

    Build-2026-01-12_14-58-06

--- STEPViewer/version.h
+++ STEPViewer/version.h
@@ -3,7 +3,7 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
 #define VERSION_PATCH 1
-#define VERSION_BUILD 2260
+#define VERSION_BUILD 2263
 
 #define STRINGIFY(x) #x
 #define STRINGIFY_HELPER(x) STRINGIFY(x)
