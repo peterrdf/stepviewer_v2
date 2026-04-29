d7a2b9a75b4d1f44b1604217b26616a3b16428a4
Peter Bonsma <peter.bonsma@rdf.bg>
2026-04-29 15:08:02 +0300

    Build-2026-04-29_15-08-01

--- STEPViewer/version.h
+++ STEPViewer/version.h
@@ -3,7 +3,7 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
 #define VERSION_PATCH 1
-#define VERSION_BUILD 2321
+#define VERSION_BUILD 2325
 
 #define STRINGIFY(x) #x
 #define STRINGIFY_HELPER(x) STRINGIFY(x)
