22e79f9708f7bab3d9d310129c4e8e3a27e9a64b
Svilen M. Varbanov <svilen.varbanov@rdf.bg>
2026-06-04 16:31:59 +0300

    Multi-threaded loading support.

--- include/_ap242_geometry.cpp
+++ include/_ap242_geometry.cpp
@@ -17,10 +17,22 @@ _ap242_geometry::_ap242_geometry(OwlInstance owlInstance, SdaiInstance sdaiInsta
     setAPFormatSettings();
 
     // Extra settings
-    setSegmentation(getSdaiModel(), 16, 0.);
+    if (getMultiThreadOwlModelWrapper() != 0) {
+        setSegmentation(getMultiThreadOwlModelWrapper(), 16, 0.);
+    }
+    else {
+        setSegmentation(getSdaiModel(), 16, 0.);
+    }
 }
 
 /*virtual*/ void _ap242_geometry::postCalculate() /*override*/
 {
-    cleanCachedGeometry();
+    _geometry::cleanCachedGeometry();
+
+    if (getMultiThreadOwlModelWrapper() != 0) {
+        cleanMemory(getMultiThreadOwlModelWrapper(), 1);
+    }
+    else {
+        cleanMemory(getSdaiModel(), 1);
+    }
 }
